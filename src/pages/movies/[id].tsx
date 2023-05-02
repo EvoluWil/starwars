63f0e14e157b5641adac6239f7cf80f69146f117
willian rodrigues <willian.rsilva1994@gmail.com>
2023-05-02 16:42:20 -0300

    fix ssr movies

--- src/pages/movies/[id].tsx
+++ src/pages/movies/[id].tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { GetStaticProps } from 'next';
+import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
 import { useTranslation } from 'react-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
@@ -55,7 +55,10 @@ const MovieDetail: React.FC<MovieDetailProps> = ({ initialData }) => {
 
 export default MovieDetail;
 
-export const getStaticProps: GetStaticProps = async ({ locale, params }) => {
+export const getServerSideProps: GetServerSideProps = async ({
+  locale,
+  params
+}) => {
   try {
     if (!params?.id) {
       throw new Error('Param is required');
